2ee833611fac280c1ff53cc168c65034d4d656d3
Luiz Felipe <luizfelipelb2018@gmail.com>
2023-03-01 18:50:48 -0300

    Alteração do site com adição de 2 sections

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     new TypeIt(".animated", {
-        speed: 350,
+        speed: 550,
         strings: "eu sou o Luiz Felipe. Desenvolvedor Full Stack e Salesforce Jr.",
         loop: true
     }).go()
